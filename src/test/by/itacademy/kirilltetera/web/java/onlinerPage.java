206431997a333ac56c0a278a5558625c2d8b6205
Kirill Tetera <kirill.v.tetera@gmail.com>
2023-02-26 19:18:06 +0300

    add onlinerPage java class

---
+++ src/test/by/itacademy/kirilltetera/web/java/onlinerPage.java
@@ -0,0 +1,8 @@
+package by.itacademy.kirilltetera.web.java;
+
+public class onlinerPage {
+    public static final String url = "https://www.onliner.by/";
+    public static final String copyright = "/html/body/div[1]/footer/div/div/div/div[2]/div[2]";
+
+
+}
